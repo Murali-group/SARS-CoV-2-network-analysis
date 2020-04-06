bd9b0b3316fe0bddfe5564dde731b27dcb159994
jlaw9 <jeffreynlaw@gmail.com>
2020-04-06 18:49:06 -0400

    git subrepo clone --branch=no-ontology git@github.com:jlaw9/FastSinkSource.git
    subrepo:
      subdir:   "FastSinkSource"
      merged:   "fa99a01"
    upstream:
      origin:   "git@github.com:jlaw9/FastSinkSource.git"
      branch:   "no-ontology"
      commit:   "fa99a01"
    git-subrepo:
      version:  "0.4.1"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "a04d8c2"

---
+++ FastSinkSource/src/algorithms/svm.py
@@ -0,0 +1,24 @@
+
+from sklearn.svm import LinearSVC
+#from sklearn.svm import SVC
+#from sklearn.calibration import CalibratedClassifierCV
+import time
+#from scikit.learn.svm.sparse import SVC
+
+
+def training(P, labels, max_iter):
+    '''
+    *P* : features corresponding to the samples in the training set
+    *labels*: labels corresponding to the samples in the training set
+    '''
+
+    clf = LinearSVC(max_iter=max_iter)
+    clf.fit(P, labels)
+
+    return clf
+
+
+def testing(clf, P):
+
+    predict = clf.decision_function(P)
+    return predict
