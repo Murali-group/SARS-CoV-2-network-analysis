ea202ec74652e1a8ecc9b73ca6e84b5cc64f256d
Nure Tasnina <tasnina@vt.edu>
2020-04-26 14:01:24 -0400

    modified run.sh

--- src/Enrichment/GO_Enrichment/run.sh
+++ src/Enrichment/GO_Enrichment/run.sh
@@ -1,3 +1,10 @@
 !#/bin/bash
-#python filter.py
+
+#filter out Krogan positive examples from output files
+python filter.py
+
+#Do GO enrichment analysis with clusterProfiler
 Rscript prediction_GoEnrichment.R
+
+#combine ouput of cluster profiler for each algorithm(separately) across all networks 
+python combine_enrichment_result.py
